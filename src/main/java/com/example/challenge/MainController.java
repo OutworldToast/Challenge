81c25c0b4a87c636487b529f7ba05d4ad3abb7f6
Sylvan <22128387@student.hhs.nl>
2022-12-01 11:39:52 +0100

    fixed merging main

---
+++ src/main/java/com/example/challenge/MainController.java
@@ -0,0 +1,22 @@
+package com.example.challenge;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+
+public class MainController {
+
+    @FXML
+    private ImageView icon;
+
+    @FXML
+    private ImageView logo;
+
+    @FXML
+    private Text usernamelabel;
+
+    @FXML
+    private Label welcomeText;
+
+}
